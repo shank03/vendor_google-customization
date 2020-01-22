0a7bfb711935df2c401b821814775ced1574349d
Dyneteve <dyneteve@pixelexperience.org>
2020-01-22 16:01:55 +0200

    Add google FI support
    * Also sort
    
    Change-Id: I1bce9d6558f2cb3b76ee5808015fd4643036e32e

--- config.mk
+++ config.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2020 The PixelExperience Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 ifeq ($(TARGET_INCLUDE_WIFI_EXT),true)
 $(call inherit-product, vendor/google/customization/interfaces/wifi_ext/wifi-ext.mk)
 endif
@@ -12,3 +28,5 @@ PRODUCT_SOONG_NAMESPACES += vendor/google/customization/apex/apex_images
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/google/customization/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/google/customization/overlay
 endif
+
+$(call inherit-product, vendor/google/customization/product/config.mk)
